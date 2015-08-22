2d6e31df6304c6d1c87f323c08189bf762780e54
Nathan Kleekamp <nkleekamp@gmail.com>
2015-08-22 10:43:06 -0400

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,97 @@
+module.exports = function(grunt) {
+    require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        sass: {
+          dist: {
+            options: {
+              style: 'compressed'
+            },
+            files: {
+              'style.css': 'style.scss'
+            }
+          }
+        },
+
+        uglify: {
+          build: {
+            files: {
+              'js/dist/modernizr.min.js': ['js/src/vendor/modernizr.min.js'],
+              'js/dist/picturefill.min.js': ['js/src/vendor/picturefill.min.js']
+            }
+          }
+        },
+
+        imagemin: {
+          dynamic: {
+            files: [
+              {
+                expand: true,
+                cwd: 'img/src/',
+                src: ['**/*.{png,jpg,gif}'],
+                dest: 'img/dist/'
+              }
+            ]
+          }
+        },
+
+        // Requires SVGO
+        svgmin: {
+          options: {
+            plugins: [
+              { collapseGroups: false }
+            ]
+          },
+          dist: {
+            files: [
+              {
+                expand: true,
+                cwd: 'img/src',
+                src: ['*.svg'],
+                dest: 'img/dist',
+                ext: '.min.svg'
+              }
+            ]
+          }
+        },
+
+        // Requires gzip
+        compress: {
+          main: {
+            options: {
+              mode: 'gzip'
+            },
+            files: [
+              {
+                expand: true,
+                cwd: 'img/dist',
+                src: ['*.svg'],
+                dest: 'img/dist',
+                ext: '.svgz'
+              }
+            ]
+          }
+        },
+        
+        watch: {
+          css: {
+            files: '**/*.scss',
+            tasks: ['sass']
+          }
+        }
+      });
+
+    grunt.task.registerTask('default', [
+      'sass',
+      'optimages',
+      'uglify'
+    ]);
+
+    grunt.task.registerTask('optimages', [
+      'imagemin',
+      'svgmin',
+      'compress'
+    ]);
+};
